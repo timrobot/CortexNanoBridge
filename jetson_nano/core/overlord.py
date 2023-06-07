53d28e6f6c0dc37067b3c7f7d2fe579346faad17
Timothy Yong <tyong_23@hotmail.com>
2023-06-07 05:19:58 -0700

    change ip fn

--- jetson_nano/core/overlord.py
+++ jetson_nano/core/overlord.py
@@ -5,20 +5,10 @@ endpoint = "http://192.168.1.34:3000"
 device_name = None
 
 def get_ip():
-    try:
-      endpoint = 'https://ipinfo.io/json'
-      response = requests.get(endpoint, verify = True)
-
-      if response.status_code == 200:
-        data = response.json()
-        IP = data['ip']
-      else:
-        raise Exception("cannot get global ip")
-    except Exception:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.settimeout(0)
-        s.connect(('10.254.254.254', 1))
-        IP = s.getsockname()[0]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    s.connect(('10.254.254.254', 1))
+    IP = s.getsockname()[0]
     return IP
 
 def set_name(name=""):
@@ -30,5 +20,5 @@ def heartbeat():
   ip = get_ip()
   if name is None:
      name = ip
-  requests.post(f"http://{endpoint}/heartbeat",
+  requests.post(f"{endpoint}/heartbeat",
     json={"name": device_name, "ipv4": ip})
