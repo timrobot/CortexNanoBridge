b2020c146121ea0583bf52ada9c5c353971eceba
Timothy Yong <tyong_23@hotmail.com>
2023-06-07 06:03:37 -0700

    rm raw arg

--- jetson_nano/test-realsense.py
+++ jetson_nano/test-realsense.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
   cam = RealsenseCamera(640, 360)
 
   while True:
-    _, color, depth = cam.read("raw")
+    _, color, depth = cam.read()
     depth_as_color = cv2.applyColorMap(np.sqrt(depth).astype(np.uint8), cv2.COLORMAP_HSV)
     # cv2.imshow("depth_as_color", depth_as_color)
     cv2.imshow("color", color)
