1b682424d96295a56d40bf21176f9a13552ca112
Timothy Yong <tyong_23@hotmail.com>
2023-06-07 06:11:31 -0700

    revamp realsense cam

--- jetson_nano/core/device.py
+++ jetson_nano/core/device.py
@@ -8,6 +8,7 @@ import os
 import rplidar
 import pyrealsense2 as rs
 from typing import Tuple
+import cv2
 from . import vex_serial, assembly
 
 PORT1  =  0
@@ -144,19 +145,39 @@ class RealsenseCamera:
   def __init__(self, width=640, height=360):
     self.width = width
     self.height = height
+    self.shape = (height, width)
+
     self.pipeline = rs.pipeline()
     
     config = rs.config()
     config.enable_stream(rs.stream.depth, self.width, self.height, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, self.width, self.height, rs.format.bgr8, 30)
     profile = self.pipeline.start(config)
+    # intel realsense on jetson nano sometimes get misdetected as 2.1 even though it has 3.2 USB
+    # profile = self.pipeline.start()
 
     depth_sensor = profile.get_device().first_depth_sensor()
     self.depth_scale = depth_sensor.get_depth_scale()
 
     self.align = rs.align(rs.stream.color)
 
-  def read(self) -> Tuple[bool, np.ndarray, np.ndarray]:
+    # self.intrinsic_matrix = self.metadata.intrinsics.intrinsic_matrix
+    # used for USB 2.1 as found on the jetson nano
+    if height == 480:
+      self.fx = 614.5665893554688
+      self.fy = 614.4674682617188
+      self.cx = 313.47930908203125
+      self.cy = 235.6346435546875
+    elif height == 360:
+      self.fx = 460.92495728
+      self.fy = 460.85058594
+      self.cx = 315.10949707
+      self.cy = 176.72598267
+
+    self.hfov = np.degrees(np.arctan2(self.width  / 2, self.fx)) * 2
+    self.vfov = np.degrees(np.arctan2(self.height / 2, self.fy)) * 2
+
+  def capture(self) -> Tuple[bool, np.ndarray, np.ndarray]:
     """
     Grab frames from the realsense camera
 
@@ -169,7 +190,7 @@ class RealsenseCamera:
     if self.pipeline:
       try:
         frames = self.pipeline.wait_for_frames()
-        aligned_frames = frames.get_depth_frame()
+        aligned_frames = self.align.process(frames)
         color_frame = aligned_frames.get_color_frame()
         depth_frame = aligned_frames.get_depth_frame()
         color_image = np.asanyarray(color_frame.get_data())
@@ -188,3 +209,24 @@ class RealsenseCamera:
     if self.pipeline:
       self.pipeline.stop()
       self.pipeline = None
+
+  def read(self): # will also store in buffer to be read
+    ret, color, depth = self.capture()
+    if not ret:
+      return np.zeros((self.height, self.width, 3), dtype=np.uint8), np.zeros((self.height, self.width), dtype=np.float32)
+    depth = depth.astype(np.float32) * self.depth_scale
+    # color = np.ascontiguousarray(np.flip(color, axis=-1))
+
+    return color, depth
+
+  def depth2rgb(self, depth):
+    if depth.dtype == np.uint16:
+      return cv2.applyColorMap(np.sqrt(depth).astype(np.uint8), cv2.COLORMAP_HSV)
+    else:
+      return cv2.applyColorMap(np.floor(np.sqrt(depth / self.depth_scale)).astype(np.uint8), cv2.COLORMAP_HSV)
+
+  def view(self):
+    color, depth = self.read()
+    combined = np.hstack((color, self.depth2rgb(depth)))
+    cv2.imshow("combined", combined)
+    cv2.waitKey(1)
