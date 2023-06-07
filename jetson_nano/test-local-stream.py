7e76a8c40f72a624886aeb2dafa4c025916c08fd
Timothy Yong <tyong_23@hotmail.com>
2023-06-07 05:30:22 -0700

    add realsense cam integration to test

--- jetson_nano/test-local-stream.py
+++ jetson_nano/test-local-stream.py
@@ -1,14 +1,17 @@
 import cv2
 from core import lan, overlord
+from core.device import RealsenseCamera
 
 if __name__ == "__main__":
-  lan.start("0.0.0.0", 9990, source=True)
+  lan.start("0.0.0.0", 9990, (360, 640, 3), source=True)
   overlord.heartbeat()
   try:
-    cam = cv2.VideoCapture(0)
+    # cam = cv2.VideoCapture(0)
+    cam = RealsenseCamera()
     while True:
-      _, frame = cam.read()
-      lan.set_frame(frame)
+      # _, frame = cam.read()
+      color, depth = cam.read()
+      lan.set_frame(color)
   except:
     pass
   finally:
