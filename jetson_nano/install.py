d10b4fef2516f9d3e3fe7493532064e98d916d39
Timothy Yong <tyong_23@hotmail.com>
2023-06-30 12:44:30 -0700

    use path join

--- jetson_nano/install.py
+++ jetson_nano/install.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     curr_dir = os.getcwd()
     with open(os.path.join(curr_dir, "scripts", "nvcortexnano.service"), "r") as fp:
-        service_lines = [line.replace("/usr/local/cortano", curr_dir + "cortano/") \
+        service_lines = [line.replace("/usr/local/cortano", os.path.join(curr_dir, "cortano")) \
                          for line in fp.readlines() if line]
     with open(os.path.join(curr_dir, "scripts", "nvcortexnano.service"), "w") as fp:
         for line in service_lines:
