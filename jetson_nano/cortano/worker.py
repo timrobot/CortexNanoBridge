3c5f9d5141b8431eea76c3f6c3452c56a2f52890
Timothy Yong <tyong_23@hotmail.com>
2023-09-22 15:33:57 -0700

    reset worker

--- jetson_nano/cortano/worker.py
+++ jetson_nano/cortano/worker.py
@@ -1,12 +1,12 @@
-from cortano import VexCortex, lan_socket
+from cortano import VexCortex, lan
 from cortano import RealsenseCamera
 
 if __name__ == "__main__":
   cam = RealsenseCamera(autostart=False)
   robot = VexCortex("/dev/ttyUSB0")
-  lan_socket.start("robot", frame_shape=(360, 640), target=robot, camera=cam)
+  lan.start("robot", frame_shape=(360, 640), target=robot, camera=cam)
 
   while robot.running():
-    lan_socket.check_alive()
+    lan.check_alive()
 
-  lan_socket.stop()
+  lan.stop()
