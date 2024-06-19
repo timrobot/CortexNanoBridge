61414058fd9ffd59f2d1bf6fcb20b1be2661f156
Timothy Yong <tyong_23@hotmail.com>
2024-06-19 05:45:53 -0700

    add voltage reading

--- jetson_nano/cortano/run-robot.py
+++ jetson_nano/cortano/run-robot.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
 
   while robot.running():
     color, depth = cam.read()
-    sensors = robot.sensors()
+    sensors, voltage = robot.sensors()
     robot.motor[0] = 0 # you can set this to any value from -127 to 127
     robot.motor[9] = 0
