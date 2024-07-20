3435a5fb769e45e8c80cd51e3e30293be4db11c5
Timothy Yong <timrobot@users.noreply.github.com>
2024-07-19 23:02:53 -0700

    Add workers for frame rate increase (#8)
    * add qoi support
    
    * add .so file as exception
    
    * fix: copy only into sized buf
    
    * fix: add lengths for the encode
    
    * fix: rm none set for color
    
    * fix: install script
    
    * improve buf query perf
    
    * feat: second cam support
    
    * fix: bytes to buf
    
    * adjust qoi deps according to platform
    
    * testing mp stream
    
    * fix int size
    
    * rm host param
    
    * add bufs back in
    
    * add debug stmts
    
    * task->worker
    
    * maxpool for increased framerate

--- jetson_nano/cortano/lan.py
+++ jetson_nano/cortano/lan.py
@@ -16,21 +16,58 @@ import websockets
 import numpy as np
 import sys
 import logging
-import os
+import cv2
 import qoi
 
 # shared variables
 frame_shape = (360, 640)
 tx_interval = 1 / 60
 
-frame_lock = Lock()
-color_buf = None
-depth_buf = None
-color2_buf = None
-color_len = Value(c_int, 0)
-depth_len = Value(c_int, 0)
-color2_len = Value(c_int, 0)
-color2_en = Value(c_bool, False)
+class RemoteByteBuf:
+  def __init__(self, size=None):
+    self.lock = Lock()
+    self.size = size
+    self.buf = None if (size is None) else RawArray(c_uint8, int(np.prod(self.size)))
+    self.len = Value(c_int, 0)
+    self.en = Value(c_bool, False)
+
+    self.encode = lambda frame: frame
+    self.decode = lambda frame: frame
+
+  def sync_process(self, lock, buf, len, en):
+    self.lock = lock
+    self.buf = buf
+    self.len = len
+    self.en = en
+
+  def numpy(self, dtype=np.uint8):
+    return np.frombuffer(self.buf, dtype=dtype)
+  
+  def enabled(self):
+    return self.en.value
+  
+  def copyfrom(self, data):
+    data = data.flatten() if isinstance(data, np.ndarray) else np.frombuffer(data, np.uint8)
+    self.lock.acquire()
+    if len(data) == np.prod(self.size):
+      np.copyto(self.numpy(), data)
+    else:
+      np.copyto(self.numpy()[:len(data)], data)
+    self.len.value = len(data)
+    self.en.value = True
+    self.lock.release()
+
+  def asbytes(self):
+    self.lock.acquire()
+    if self.en.value == True:
+      bytestr = self.numpy()[:self.len.value].tobytes()
+      self.en.value = False
+    else:
+      bytestr = None
+    self.lock.release()
+    return bytestr
+
+_color_encoding_parameters = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
 motor_values = None
 sensor_values = None # [voltage, sensor1, sensor2, ...]
@@ -39,62 +76,99 @@ sensor_length = Value(c_int, 0)
 main_loop = None
 _running = Value(c_bool, True)
 last_rx_time = Array(c_char, 100)
-comms_task = None
+_stream_host = Array(c_int, 4)
+
+rxtx_task = None
 send_task1 = None
 send_task2 = None
-recv_task = None
+send_task3 = None
+
+color_buf = None
+depth_buf = None
+color_buf2 = None
+
+# because the jetson doesn't properly support wchar
+def ip2l(x): return [int(b) for b in x.split('.')]
+def l2ip(x): return "%d.%d.%d.%d" % tuple(x)
+
+async def streamer(port, msg_buf):
+  while _running.value:
+    if not msg_buf.enabled():
+      await asyncio.sleep(0.01)
+      continue
+
+    _stream_host.acquire()
+    host = l2ip(_stream_host) # always update to the latest ipv4 host
+    _stream_host.release()
+    try:
+      if host != "0.0.0.0":
+        msg = msg_buf.asbytes()
+        async with websockets.connect("ws://" + host + ":" + str(port)) as websocket:
+          await websocket.send(msg)
+    except Exception as e:
+      await asyncio.sleep(0.01)
+
+def streamer_worker(host, port, run, lock, buf, len, en):
+  global main_loop, _running, _stream_host
+  msg_buf = RemoteByteBuf()
+  msg_buf.sync_process(lock, buf, len, en)
+  _stream_host = host
+
+  _running = run
+  main_loop = asyncio.new_event_loop()
+  stream_task = main_loop.create_task(streamer(port, msg_buf))
+
+  for signo in [signal.SIGINT, signal.SIGTERM]:
+    main_loop.add_signal_handler(signo, stream_task.cancel)
+  
+  try:
+    asyncio.set_event_loop(main_loop)
+    main_loop.run_until_complete(stream_task)
+  except (KeyboardInterrupt,):
+    _running.value = False
+    main_loop.stop()
+  finally:
+    main_loop.run_until_complete(main_loop.shutdown_asyncgens())
+    main_loop.close()
 
 async def sender(websocket):
-  # we can use this in order to prevent data transfer latencies or data synchronization issues
-  color_np = np.frombuffer(color_buf, np.uint8)
-  depth_np = np.frombuffer(depth_buf, np.uint8)
-  color2_np = np.frombuffer(color2_buf, np.uint8)
-  # last_tx_time = None
+  last_tx_time = None
 
   while _running.value:
     try:
-      # throttle communication so that we don't bombard the socket connection
-      curr_time = datetime.now()
-      # dt = tx_interval if last_tx_time is None else (curr_time - last_tx_time).total_seconds()
-      # if dt < tx_interval:
-      #   await asyncio.sleep(tx_interval - dt)
-      #   last_tx_time = datetime.now()
-      # else:
-      #   last_tx_time = curr_time
-
-      frames = []
-      frame_len = []
-      frame_lock.acquire()
-      frame_len = [color_len.value, depth_len.value]
-      frames = [color_np[:color_len.value].tobytes(), depth_np[:depth_len.value].tobytes()]
-      # tobytes() might be slow but hopefully good enough
-      if color2_en.value:
-        frame_len.append(color2_len.value)
-        frames.append(color2_np[:color2_len.value].tobytes())
-      frame_lock.release()
+      curr_time = time.time()
+      dt = tx_interval if last_tx_time is None else (curr_time - last_tx_time)
+      if dt < tx_interval:
+        await asyncio.sleep(tx_interval - dt) # throttle to prevent overload
+        last_tx_time = time.time()
+      else:
+        last_tx_time = curr_time
 
       sensor_values.acquire()
       sensors = sensor_values[:sensor_length.value]
       sensor_values.release()
 
       msg = json.dumps({
-        "lengths": frame_len,
-        "timestamp": datetime.isoformat(curr_time),
+        "timestamp": datetime.isoformat(datetime.now()),
         "sensors": [int(x) for x in sensors[1:]],
         "voltage": int(sensors[0])
-      }).encode()
-
-      await websocket.send([msg] + frames) # sent as an iterable to improve performance
+      })
+      await websocket.send(msg)
     except websockets.ConnectionClosed:
-      logging.warning(datetime.isoformat(datetime.now()) + " Connection closed.")
-      # last_tx_time = None
-      await asyncio.sleep(1)
+      # logging.warning(datetime.isoformat(datetime.now()) + " Connection closed.")
+      last_tx_time = None
+      await asyncio.sleep(1) # just wait forever...
 
 async def receiver(websocket):
   while _running.value:
     try:
       msg = await websocket.recv()
-      motors = json.loads(msg.decode("utf-8"))["motors"]
+      msg = json.loads(msg.decode("utf-8"))
+      _stream_host.acquire()
+      _stream_host[:] = ip2l(msg["ipv4"])
+      _stream_host.release()
+
+      motors = msg["motors"]
       if len(motors) == 10:
         motor_values.acquire()
         motor_values[:] = motors
@@ -102,30 +176,23 @@ async def receiver(websocket):
         last_rx_time.acquire()
         last_rx_time.value = datetime.isoformat(datetime.now()).encode()
         last_rx_time.release()
+
     except ValueError:
       logging.error("Invalid data received:", msg)
     except websockets.ConnectionClosed:
-      logging.warning(datetime.isoformat(datetime.now()) + " Connection closed.")
+      # logging.warning(datetime.isoformat(datetime.now()) + " Connection closed.")
       motor_values.acquire()
       motor_values[:] = [0] * 10
       motor_values.release()
-      # last_rx_time.acquire()
-      # last_rx_time.value = "".encode()
-      # last_rx_time.release()
       await asyncio.sleep(1)
 
-async def handle_send(websocket, path):
-  global send_task
-  send_task = main_loop.create_task(sender(websocket))
-  await asyncio.gather(send_task)
-
-async def handle_recv(websocket, path):
-  global recv_task
+async def handle_rxtx(websocket, path):
   recv_task = main_loop.create_task(receiver(websocket))
-  await asyncio.gather(recv_task)
+  send_task = main_loop.create_task(sender(websocket))
+  await asyncio.gather(recv_task, send_task)
 
-async def request_handler(host, port, coro):
-  async with websockets.serve(coro, host, port):
+async def request_handler(host, port):
+  async with websockets.serve(handle_rxtx, host, port):
     try:
       await asyncio.Future()
     except asyncio.exceptions.CancelledError:
@@ -135,48 +202,20 @@ async def request_handler(host, port, coro):
       logging.error(e)
       sys.exit(1)
 
-def recv_worker(port, run, mvals):
-  global main_loop, _running
-  global motor_values
+def rxtx_worker(host, port, run, mvals, svals, slen):
+  global main_loop, _running, _stream_host
+  global motor_values, sensor_values, sensor_length
   motor_values = mvals
-
-  _running = run
-  main_loop = asyncio.new_event_loop()
-  recv_task = main_loop.create_task(request_handler("0.0.0.0", port, handle_recv))
-  try:
-    asyncio.set_event_loop(main_loop)
-    main_loop.run_until_complete(recv_task)
-  except (KeyboardInterrupt,):
-    _running.value = False
-    main_loop.stop()
-  finally:
-    main_loop.run_until_complete(main_loop.shutdown_asyncgens())
-    main_loop.close()
-
-def send_worker(port, run, flock, cbuf, clen, dbuf, dlen, cbuf2, clen2, c2en, svals, ns):
-  global main_loop, _running
-  global color_buf, color_len, depth_buf, depth_len, frame_lock
-  global color2_buf, color2_len, color2_en
-  global sensor_values, sensor_length
-
-  frame_lock = flock
-  color_buf = cbuf
-  color_len = clen
-  depth_buf = dbuf
-  depth_len = dlen
-  color2_buf = cbuf2
-  color2_len = clen2
-  color2_en = c2en
-
   sensor_values = svals
-  sensor_length = ns
+  sensor_length = slen
+  _stream_host = host
 
   _running = run
   main_loop = asyncio.new_event_loop()
-  send_task = main_loop.create_task(request_handler("0.0.0.0", port, handle_send))
+  rxtx_task = main_loop.create_task(request_handler("0.0.0.0", port))
   try:
     asyncio.set_event_loop(main_loop)
-    main_loop.run_until_complete(send_task)
+    main_loop.run_until_complete(rxtx_task)
   except (KeyboardInterrupt,):
     _running.value = False
     main_loop.stop()
@@ -193,10 +232,9 @@ def start(port=9999, robot=None):
       realsense (_type_, optional): _description_. Defaults to None.
       secondaryCam (bool, optional): Reserve the /dev/video* cam port on LAN. Don't set to True if you already opened a camera. Defaults to False.
   """
-  global comms_task
   global robot_entity, motor_values, sensor_values, sensor_length
-  global color_buf, depth_buf, color2_buf
-  global recv_task, send_task1, send_task2
+  global color_buf, depth_buf, color_buf2
+  global rxtx_task, send_task1, send_task2, send_task3
   robot_entity = robot
 
   if robot_entity is not None:
@@ -209,36 +247,38 @@ def start(port=9999, robot=None):
     sensor_length.value = 1
 
   # we are sending over encoded strings + termination char
-  max_size = lambda h, w, c: 14 + h * w * (c + 1) + 8 + 2
-  color_buf = RawArray(c_uint8, max_size(frame_shape[0], frame_shape[1], 3))
-  depth_buf = RawArray(c_uint8, max_size(frame_shape[0], frame_shape[1] // 2, 4))
-  color2_buf = RawArray(c_uint8, max_size(frame_shape[0], frame_shape[1], 3))
+  qoi_size = lambda h, w, c: 14 + h * w * (c + 1) + 8 + 2
+  cbuf = RemoteByteBuf(np.prod((frame_shape[0], frame_shape[1], 3)))
+  dbuf = RemoteByteBuf(qoi_size(frame_shape[0], frame_shape[1] // 2, 4))
+  cbuf2 = RemoteByteBuf(np.prod((frame_shape[0], frame_shape[1], 3)))
 
-  recv_task = Process(target=recv_worker, args=(
-    port, _running, motor_values))
-  recv_task.start()
+  rxtx_task = Process(target=rxtx_worker, args=(
+    _stream_host, port, _running, motor_values, sensor_values, sensor_length))
+  rxtx_task.start()
 
-  send_task1 = Process(target=send_worker, args=(
-    port-1, _running,
-    frame_lock, color_buf, color_len, depth_buf, depth_len,
-    color2_buf, color2_len, color2_en,
-    sensor_values, sensor_length))
+  send_task1 = Process(target=streamer_worker, args=(
+    _stream_host, port-1, _running, cbuf.lock, cbuf.buf, cbuf.len, cbuf.en))
+  send_task2 = Process(target=streamer_worker, args=(
+    _stream_host, port-2, _running, dbuf.lock, dbuf.buf, dbuf.len, dbuf.en))
+  send_task3 = Process(target=streamer_worker, args=(
+    _stream_host, port-3, _running, cbuf2.lock, cbuf2.buf, cbuf2.len, cbuf2.en))
+
   send_task1.start()
-
-  send_task2 = Process(target=send_worker, args=(
-    port-2, _running,
-    frame_lock, color_buf, color_len, depth_buf, depth_len,
-    color2_buf, color2_len, color2_en,
-    sensor_values, sensor_length))
   send_task2.start()
+  send_task3.start()
+
+  color_buf = cbuf
+  depth_buf = dbuf
+  color_buf2 = cbuf2
 
 def stop():
-  global send_task1, send_task2, recv_task
+  global send_task1, send_task2, send_task3, rxtx_task
   _running.value = False
   time.sleep(0.3)
   send_task1.kill()
   send_task2.kill()
-  recv_task.kill()
+  send_task3.kill()
+  rxtx_task.kill()
 
 def set_frames(color: np.ndarray=None, depth: np.ndarray=None, color2: np.ndarray=None):
   """Set the outgoing color, depth frames
@@ -248,29 +288,19 @@ def set_frames(color: np.ndarray=None, depth: np.ndarray=None, color2: np.ndarra
       depth (np.ndarray, optional): Realsense Camera depth frame. Defaults to None.
       color2 (np.ndarray, optional): second camera color frame. Defaults to None.
   """
-  h, w = frame_shape
-  color_np = np.frombuffer(color_buf, np.uint8)
-  depth_np = np.frombuffer(depth_buf, np.uint8)
-  color2_np = np.frombuffer(color2_buf, np.uint8)
   if color is not None and depth is not None:
-    color = qoi.encode(color)
+    color = pickle.dumps(cv2.imencode('.jpg', color, _color_encoding_parameters), 0)
+    h, w = depth.shape
+    depth = depth.reshape((h // 2, 2, w // 2, 2)).max((1, 3)) # 2x2 maxpool
+    h, w = depth.shape
     depth = qoi.encode(depth.view(np.uint8).reshape((h, w // 2, 4)))
   if color2 is not None:
-    color2_en.value = True
-    color2 = qoi.encode(color2)
-  frame_lock.acquire()
+    color2 = pickle.dumps(cv2.imencode('.jpg', color2, _color_encoding_parameters), 0)
   if color is not None and depth is not None:
-    np.copyto(color_np[:len(color)], np.frombuffer(color, np.uint8))
-    color_np[len(color)] = 0
-    np.copyto(depth_np[:len(depth)], np.frombuffer(depth, np.uint8))
-    depth_np[len(depth)] = 0
-    color_len.value = len(color)
-    depth_len.value = len(depth)
+    color_buf.copyfrom(color)
+    depth_buf.copyfrom(depth)
   if color2 is not None:
-    np.copyto(color2_np[:len(color2)], np.frombuffer(color2, np.uint8))
-    color2_np[len(color2)] = 0
-    color2_len.value = len(color2)
-  frame_lock.release()
+    color_buf2.copyfrom(color2)
 
 def write(values, voltage_level=None):
   """Send sensor values and voltage to remote location
