c16c1aed9cd0f9feece7008f561c1aac1e2d74d9
Timothy Yong <tyong_23@hotmail.com>
2024-07-24 22:48:31 -0700

    change value range in the comments

--- jetson_nano/cortano/run-robot.py
+++ jetson_nano/cortano/run-robot.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
   while robot.running():
     color, depth = realsense.read()
     sensors, battery = robot.sensors()
-    robot.motor[0] = 0 # you can set this to any value from -1 to 1
+    robot.motor[0] = 0 # you can set this to any value from -100 to 100
     robot.motor[9] = 0
