fa1c9080363fbe2ba6919b0daa963afd63bcd316
Timothy Yong <tyong_23@hotmail.com>
2024-08-08 06:12:56 -0700

    add missing component for the v5 brain service

--- jetson_nano/install.py
+++ jetson_nano/install.py
@@ -9,3 +9,9 @@ if __name__ == "__main__":
     with open(os.path.join(curr_dir, "scripts", "nvcortexnano.service"), "w") as fp:
         for line in service_lines:
              fp.write(line + "\n")
+    with open(os.path.join(curr_dir, "scripts", "nvv5nano.service"), "r") as fp:
+        service_lines = [line.replace("/usr/local/cortano", os.path.join(curr_dir, "cortano")) \
+                         for line in fp.readlines() if line]
+    with open(os.path.join(curr_dir, "scripts", "nvv5nano.service"), "w") as fp:
+        for line in service_lines:
+             fp.write(line + "\n")
